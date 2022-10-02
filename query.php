b2c2fa384bc2dace02be2aaea031557270331c49
hasan sohib <aresuma307@gmail.com>
2022-10-02 17:09:14 +0700

    anjayyyyy

---
+++ query.php
@@ -0,0 +1,30 @@
+<?php
+class crud extends koneksi 
+{
+    public function lihatData()
+    {
+    $sql ="SELECT * FROM user_detail";
+    $result= $this->koneksi->prepare($sql);
+    $result->excute();
+    return $result;
+}
+public function insertData($email, $pass, $name)
+{
+    try
+    {
+        $sql="INSERT INTO user_detail(user_email, user_password, user_fullname, level) VALUES (:email, :pass, :name, 2)";
+        $result=$this->koneksi->prepare($sql);
+        $result->bindParam(":email",$email);
+        $result->bindParam(":pass",$pass);
+        $result->bindParam(":name", $name);
+        $result->extends();
+        return true;
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+        return false;
+    }
+}
+}
+?>
---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+require ('koneksi.php');
+require ('query.php');
+$obj = new crud;
+if ( isset($_POST['register'])){
+    $userMail = $_POST['txt_email'];
+    $userpass = $_POST['txt_pass'];
+    $userName = $_POST['txt_nama'];
+
+    $query = "INSERT INTO user_detail VALEUS('','$userMail','$userpass','$userName',2)";
+    $result = mysqli_query($koneksi , $query);
+    header('location: login.php');
+}
+?>
+
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <form action="register.php" method="POST">
+            <P>email  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <input type="text" name="txt_email" required></P>
+            <P>password : <input type="password" name="text_pass" required></P>
+            <P>nama  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <input type="text" name="txt_nama" required></P>
+        </form>
+        <p><a href="login.php">Login</p>
+    </body>
+</html>
